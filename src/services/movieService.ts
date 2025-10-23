f1eec98a370b1fef18176641526ec728f6f131c2
Torryy00 <votakmasha@gmail.com>
2025-10-23 17:01:26 +0200

    commit

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,27 +1,26 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-export interface MovieHttpResponse {
+const BASE_URL = "https://api.themoviedb.org/3";
+const ACCESS_TOKEN = import.meta.env.VITE_TMDB_TOKEN as string;
+
+interface FetchMoviesResponse {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
 }
 
-const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+export async function fetchMovies(query: string, page: number = 1): Promise<FetchMoviesResponse> {
+  const { data } = await axios.get<FetchMoviesResponse>(`${BASE_URL}/search/movie`, {
+    params: {
+      query,
+      page,
+    },
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  });
 
-export const fetchMovies = async (
-  query: string,
-  page: number
-): Promise<MovieHttpResponse> => {
-  const response = await axios.get<MovieHttpResponse>(
-    "https://api.themoviedb.org/3/search/movie",
-    {
-      params: { query, page },
-      headers: {
-        Authorization: `Bearer ${TMDB_TOKEN}`,
-      },
-    }
-  );
-  return response.data;
-};
+  return data;
+}
